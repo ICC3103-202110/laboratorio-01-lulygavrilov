d546a856e634868d75a754512f53a59e7a2320cd
Luly Gavrilov <lulygavrilov@Lulys-MacBook-Pro.local>
2021-03-26 09:03:00 -0300

    commit

--- lab_1.py
+++ lab_1.py
@@ -1,14 +1,15 @@
 import random
 
-#solicitar el número de cartas a jugar
-#una carta es un número
-#si los jugadores quieren jugar 20 cartas, 
-#debe generar 20 pares con numeros del 1 al 20
+#Inicio del juego
 
-#Cada jugador inicia con 0 puntos
+print("\n . : ¡Let's play Memorize! : . \n")
 
-print(". : ¡Let's play Memorize! : .")
-size = int(input("With how many cards would you like to play?:"))
+#Creación del tablero con cartas
+
+size = int(input("- With how many cards would you like to play? -: "))
+while size <0:
+    print("- Please enter a valid number-")
+    size = int(input("-With how many cards would you like to play?-"))
 
 def board(size):
   board = []
@@ -21,4 +22,37 @@ def board(size):
     censor.append('X ')
   return censor
 
+print("\n. : Perfect! Lets begin. : . \n . : This is your board : .\n")
 print (board(size))
+
+scorep1 = 0
+scorep2 = 0
+keep_playing = 0 #Escribimos esto como condición para el loop del juego
+
+while keep_playing == 0:
+    a = '1'
+    coords1 = input("\n-Player " + a + " select your card's coordinates. (Example: 0,1)-: ")
+
+
+
+
+
+
+    condition_to_continue = input("\n- Do you wish to keep playing? <Yes/No> - :")
+    if condition_to_continue == 'No' or condition_to_continue == 'no':
+        keep_playing = 1
+    elif condition_to_continue == 'Yes' or condition_to_continue == 'yes':
+        keep_playing = 0
+    else:
+        print("-The answer was not valid. Play another round and try again!-")
+
+if scorep1 > scorep2:
+    print("\n -Player 1 wins by .:" + str(scorep1 - scorep2) + ":. points, with a score of" + str(scorep1) + "!-")
+elif scorep1 == scorep2:
+    print("\n -¡Its a Draw! You guys matched with .:" + str(scorep1) + ":. points!-")
+else:
+    print("\n -Player 2 wins by .:" + str(scorep2 - scorep1) + ":. points, with a score of" + str(scorep2) + "!-")
+
+print("\n. : Wohoo what a great game! Thank you for playing please come back soon. : .")
+
+
